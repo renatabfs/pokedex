bc4ebc2a08047bb2c1c90f4b7fab22b5cb901bbc
renatabfs <renata.niall66@gmail.com>
2023-02-24 20:31:27 -0300

    adding button next and prev functions

--- js/script.js
+++ js/script.js
@@ -36,3 +36,22 @@ const renderPokemon = async (pokemon) =>{
     pokemonImage.style.display ='none';
 }
 }
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase())
+})
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon)
+    }
+})
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon)
+})
+
+renderPokemon(searchPokemon)
